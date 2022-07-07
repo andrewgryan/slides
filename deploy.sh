449c1afddae9716d1c2ba4a0d486f74212998cd2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-07-07 13:14:59 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ cd hello-world
 npx slidev build --out ../dist/hello-world --base /slides/hello-world/
 npx slidev build --out ../dist/pytest-primer --base /slides/pytest-primer/ pytest-primer.md
 npx slidev build --out ../dist/git-primer --base /slides/git-primer/ git-primer.md
+npx slidev build --out ../dist/forest --base /slides/forest/ forest.md
 cd -
 
 # Enter static HTML directory
